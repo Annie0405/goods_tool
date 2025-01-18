41d61a0251748085e9e88d47fd71f3ccd1069743
shiyinwulv <shiyinwulv@twinkleway.com>
2025-01-18 14:18:44 +0800

    备份

---
+++ main.py
@@ -0,0 +1,18 @@
+from groupbuytools.group_buy_tools import GroupBuyTools
+
+# rijiubajishuizu = GroupBuyTools(r"datas/日九系列/日九吧唧水组")
+# rijiubajishuizu.verify()
+# rijiubajishuizu.gen_final_price()
+#
+# rijiubajifengzu = GroupBuyTools(r"datas/日九系列/日九吧唧风组")
+# rijiubajifengzu.verify()
+# rijiubajifengzu.gen_final_price()
+
+rijiupld = GroupBuyTools(r"datas/日九系列/日九拍立得")
+rijiupld.verify()
+# rijiupld.gen_final_price()
+rijiupld.visualize_matching_table()
+
+# guancangka = GroupBuyTools(r"datas/馆藏卡")
+# guancangka.verify()
+# guancangka.cal_remaining()
