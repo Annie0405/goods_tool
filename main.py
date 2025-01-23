ba535fc1c81f4767b0357a71fa1de217c2afcbe4
Annie0405 <1437364838@qq.com>
2025-01-23 21:39:35 +0800

    备份

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ rijiupld = GroupBuyTools(r"datas/日九系列/日九拍立得")
 guancangka = GroupBuyTools(r"datas/馆藏卡")
 tripxiaoka = GroupBuyTools(r"datas/trip系列/trip小卡")
 
-rijiubajifengzu.verify()
-rijiubajifengzu.gen_final_price()
-rijiubajifengzu.cal_pay_table()
+rijiupld.verify()
+rijiupld.cal_pay_table()
