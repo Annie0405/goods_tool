49b2dbbc5c4a9312549714b6ea4be533e77b04d0
shiyinwulv <shiyinwulv@twinkleway.com>
2025-02-22 14:49:00 +0800

    备份

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from groupbuytools.group_buy_tools import GroupBuyTools
 from groupbuytools.indeterminacy_mix import IMTools
 
 v5_A = IMTools(r"datas/v5系列/v5花前")
-# v5_A.cal_pay_table()
+v5_A.cal_pay_table()
 
 v5_B = IMTools(r"datas/v5系列/v5花后")
 v5_B.cal_pay_table()
